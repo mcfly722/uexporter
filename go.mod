d09b65f25e903e045334ce257dfa94800fe08e45
mcfly722 <7221798@gmail.com>
2022-07-10 01:03:16 +0300

    scheduler+exec

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/dop251/goja v0.0.0-20220516123900-4418d4575a41
 	github.com/gorilla/mux v1.8.0
 	github.com/mcfly722/goPackages/context v0.0.0-20220626121949-38712136951f
-	github.com/mcfly722/goPackages/jsEngine v0.0.0-20220709212919-e94802412e22
+	github.com/mcfly722/goPackages/jsEngine v0.0.0-20220709214858-9b64b3501881
 	github.com/mcfly722/goPackages/plugins v0.0.0-20220626121949-38712136951f
 	gopkg.in/yaml.v2 v2.4.0
 )
--- plugins/test1/scripts/test.js
+++ plugins/test1/scripts/test.js
@@ -1,35 +1,21 @@
 
 var ticker = Scheduler.NewTicker(1*1000, function(){
-	Console.Log("timer1")
+
+
+	Console.Log("exec!")
+
+	var output = ""
+
+	function onStdout(content){
+		  output+=content+'\r\n'
+	}
+
+	function onDone(content){
+		Console.Log("content="+output)
+	}
+
+	Exec.NewCommand("ping.exe", ["-n","2", "localhost"]).SetTimeoutMs(900).SetOnStdoutString(onStdout).SetOnDone(onDone).Start()
+
 }).Start()
 
 Console.Log("timer started")
-
-
-/*
-var count1 = 0
-timerId1 = setInterval(function(){
-	Console.log("timer1="+count1)
-
-	if (count1 > 5) {
-		Console.log("clearInterval("+timerId1+")")
-		clearInterval(timerId1)
-	}
-	               1/0
-	count1++
-},1000,500)
-
-var count2 = 0
-timerId2 = setInterval(function(){
-	Console.log("timer2="+count2)
-
-	if (count2 > 5) {
-		Console.log("clearInterval("+timerId2+")")
-		clearInterval(timerId2)
-	}
-
-	count2++
-},600,500)
-
-clearInterval(23)
-*/
