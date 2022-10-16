c91a21b9e2459555a9df6d4ef4135a9503ec1a2e
mcfly722 <7221798@gmail.com>
2022-10-16 17:42:54 +0300

    uptime.js

---
+++ plugins/uptime.js
@@ -0,0 +1,28 @@
+var procPath = "/proc"
+
+if (OS.Getenv("UEXPORTER_PROC_PATH") !== "") {
+  procPath = OS.Getenv("UEXPORTER_PROC_PATH")
+}
+
+var hostname = OS.Getenv("UEXPORTER_HOST_NAME")
+if (!hostname) {
+  hostname = IOUtil.ReadAll(procPath + "/sys/kernel/hostname").trim()
+}
+
+function getUptime(){
+   return (IOUtil.ReadAll(procPath+"/uptime").split(" "))[0].trim()
+}
+
+var ticker = Scheduler.NewTicker(3109, function(){
+
+  try {
+
+    out = 'uptime{host="' + hostname + '"} ' + getUptime() + "\n"
+
+    UExporter.Publish(out)
+
+  } catch(e) {
+    UExporter.Publish("# exception:"+e)
+  }
+
+}).Start()
